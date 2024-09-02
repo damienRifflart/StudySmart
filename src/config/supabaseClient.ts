c2ec4bfa041a644ffd8594c800bd233a4fb7e8b1
Dams <rifflartdamien68@gmail.com>
2024-09-02 21:43:36 +0200

    v2.0.0

--- src/config/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://fnjrkrpnfmfvdxdkuqmf.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
